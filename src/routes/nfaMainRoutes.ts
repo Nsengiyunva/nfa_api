d4dfedfc5ff314849f84bcda4dbff2d3e0c30ea6
Nsengiyunva <kingbecks07@gmail.com>
2025-12-01 15:52:10 +0300

    updates  the  app

--- src/routes/nfaMainRoutes.ts
+++ src/routes/nfaMainRoutes.ts
@@ -65,6 +65,10 @@ router.delete("/:id", async (req: Request, res: Response) => {
 });
 
 router.get("/stats/dashboard", getDashboard);
-router.get("/farmers/all", fetchFarmers );
+router.get("all/farmers", async(  req: Request, res: Response ) => {
+  res.status(200).json( {
+    message: "Test"
+  } );
+} );
 
 export default router;
