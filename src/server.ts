b1f0d7d83917a7ebfe9632d51c0d95b34e26b756
Nsengiyunva <kingbecks07@gmail.com>
2025-12-01 09:22:12 +0300

    updates the nfa api

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/farmers", farmerRoutes);
+app.use("/api/farmers", nfaMainRoutes);
 
 console.log("JWT_SECRET:", process.env.JWT_SECRET);
 
